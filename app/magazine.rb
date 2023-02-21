aa960a45acc4956ff867cb88545df994f6fd73c2
Omari99 <mariam.ali01@student.moringaschool.com>
2023-02-21 21:07:52 +0300

    add app

---
+++ app/magazine.rb
@@ -0,0 +1,52 @@
+require_relative './article'
+
+class Magazine
+  attr_accessor :name, :category
+
+  @@all = []
+
+  def initialize(name, category)
+    @name = name
+    @category = category
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def contributors
+    #array of Author instances who have written for the magazine
+    articles_for_magazine.map do |filtered_article|
+      filtered_article.author
+    end
+  end
+
+  def self.find_by_name(magazine_name)
+    #first magazine object that matches
+    Magazine.all.find do |magazine|
+      magazine.name == magazine_name
+    end
+  end
+
+  def article_titles
+    #returns an array strings of the titles of all articles written for that magazine
+    articles_for_magazine.map do |filtered_article|
+      filtered_article.title
+    end
+  end
+
+  def contributing_authors
+    #returns an array of authors who have written more than 2 articles for the magazine
+    self.contributors.filter do |author|
+      author.articles.count > 2
+    end
+  end
+   private
+#use self.
+  def articles_for_magazine
+    Article.all.filter do |article|
+      article.magazine == self
+    end
+  end
+end
